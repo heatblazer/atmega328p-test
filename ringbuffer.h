b0a193cede9c702b010fa5e6c38cfb4ad2044f72
ilian zapryanov <ilianzapryanov@aol.com>
2018-10-23 10:28:37 +0300

    fixed minor bug

--- ringbuffer.h
+++ ringbuffer.h
@@ -1,20 +1,19 @@
 
-#define T_RING_BUFFER(T, N, S) 			\
-    static T N##arr[(S)] = {0}; 		\
-    static unsigned short N##_m_size  = (S);	\
-    static unsigned short N##_rhead = 0; 	\
-    static unsigned short N##_whead =0;  	\
-    static void N##_init(void) 			\
-    {						\
-    }						\
-    static void N##_get_data(T* ret)		\
-    {						\
-	if (N##_rhead < N##_whead)		\
-	    *ret = N##arr[N##_rhead++ & S-1];	\
-    }						\
-    static void N##_put_data(T data)		\
-    {						\
-	if (N##_whead < N##_m_size &&		\
-	    N##_rhead <= N##_whead)		\
-        N##arr[N##_whead++ & S-1] = data;	\
+#define T_RING_BUFFER(T, N, S) 			 	\
+    static T N##arr[(S)] = {0}; 		 	\
+    static unsigned short N##_m_size  = (S);	 	\
+    static unsigned short N##_rhead = 0; 	 	\
+    static unsigned short N##_whead =0;  	 	\
+    static void N##_init(void) 			 	\
+    {						 	\
+    }						 	\
+    static void N##_get_data(T* ret)		 	\
+    {						 	\
+	if (N##_rhead < N##_whead)		 	\
+	    *ret = N##arr[N##_rhead++ & S-1];	 	\
+    }						 	\
+    static void N##_put_data(T data)		 	\
+    {						 	\
+	if (N##_rhead < N##_whead)		 	\
+            N##arr[N##_whead++ & S-1] = data;	 	\
     }						
